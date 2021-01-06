0dd0395e85dbea98c2ba03a70e308f6f5ad8160f
abdulhafizr <abdulhafizramadan.17@gmail.com>
2021-01-06 19:55:00 +0700

    Refactor code to atomic design

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { HomePage } from './pages';
+
+export default App = ()=> {
+  return (
+    <View style={{flex: 1}}>
+      <HomePage />
+    </View>
+  );
+};
---
+++ src/pages/HomePage/styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: 'black'
+  },
+  mainApp: {
+    flex: 1,
+    backgroundColor: 'black'
+  },
+  buttonNavigationWrapper: {
+    height: 54,
+    backgroundColor: 'white',
+    justifyContent: 'center'
+  },
+  buttonNavigation: {
+    flexDirection: 'row',
+  },
+  iconButtonNavigation: {
+    flex: 1,
+    alignItems: 'center',
+  }
+});
