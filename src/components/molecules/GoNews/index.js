185bb44c49131ab1a8944de5903e88ade7478360
abdulhafizr <abdulhafizramadan.17@gmail.com>
2021-01-10 14:11:38 +0700

    Make Fitur GoNews at src/components/GoNews

---
+++ src/components/molecules/GoNews/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+export default GoNews = () => {
+	return (
+		<View>
+			<Text>GoNews</Text>
+		</View>
+	)
+}
+ 
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -3,5 +3,6 @@ import MainApp from './MainApp';
 import AppBar from './AppBar';
 import Gopay from './Gopay';
 import Features from './Features';
+import GoNews from './GoNews';
 
-export { AppBar, ButtonNavigation, MainApp, Gopay, Features };
+export { AppBar, ButtonNavigation, MainApp, Gopay, Features, GoNews };
