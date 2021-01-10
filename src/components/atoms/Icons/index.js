949a9bf8ddcb9768e20eca6238fe367a96cda9db
abdulhafizr <abdulhafizramadan.17@gmail.com>
2021-01-10 09:23:29 +0700

    Make directory Icons in src/components/atoms/Icons

---
+++ src/components/atoms/Icons/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default Icon = () => {
+	return (
+		<View>
+			<Text>Icons</Text>
+		</View>
+	)
+}
--- src/components/molecules/MainApp/index.js
+++ src/components/molecules/MainApp/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import AppBar from '../AppBar';
 import Gopay from '../Gopay';
+import { Icons } from '../../atoms'; 
 import styles from './styles';
 
 export default MainApp = () => {
@@ -9,6 +10,7 @@ export default MainApp = () => {
 		<View style={styles.mainApp}>
 			<AppBar />
 			<Gopay />
+			<Icons  />
 		</View>
 	)
 }
