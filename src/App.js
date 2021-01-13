918d2c764ee947522f41363575ee8e7cdf73e6fc
abdulhafizr <abdulhafizramadan.17@gmail.com>
2021-01-13 19:44:17 +0700

    Restructure atomic design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import { HomePage } from './pages';
+import { HomePage } from './containers/pages';
 
 export default App = ()=> {
   return (
--- src/containers/pages/HomePage/index.js
+++ src/containers/pages/HomePage/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { ButtonNavigation, MainApp } from '../../components';
+import { ButtonNavigation, MainApp } from '../../../components';
 import styles from './styles';
 
 export default HomePage = () => {
